0d14f13a23110305090425f71cdbc7daa7555277
winnie921103 <winnie921103@gmail.com>
2020-03-24 15:38:07 +0800

    test

--- js/custom.js
+++ js/custom.js
@@ -49,6 +49,11 @@
             $(this).toggleClass("checked");
 
         });
+        window.addEventListener('resize', () => {
+            // We execute the same script as before
+            let vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        });
 
     });
 
